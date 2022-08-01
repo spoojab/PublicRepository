88ffc91d8600484cc6473c4727beae37884e7df8
spoojab <Poojasharma120899@gmail.com>
2022-08-01 14:17:55 +0530

    logger added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val TAG_THREE = "three"
+        Log.d(TAG_THREE,"Logger two added")
     }
 }
